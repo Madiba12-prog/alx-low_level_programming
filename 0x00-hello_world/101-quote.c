4a23b86a9b851185462a4b988c984cf2c867ecbc
Madan <madanbeshir496@gmail.com>
2022-09-08 14:13:09 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-gcc -S masm=intel $CFILE
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	write(2, "and that piece if art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	return (1);
+}
