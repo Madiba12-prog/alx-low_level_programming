b1efd0f6c5eb67feb71b65aa1a114b3ac6c8b1d2
Madan <madanbeshir496@gmail.com>
2022-09-14 06:55:20 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,3 +1,11 @@
+#include "0-putchar.c"
+
+/**
+* main - prints putchar
+*
+*Return: Always 0 (Success)
+*/
+
 int main(void)
 {
 	_putchar('_');
@@ -9,6 +17,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
-	return(0);
+	return (0);
 }
