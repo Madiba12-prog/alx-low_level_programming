50f927451f58f9267d77ecc53b869f0afb8b6b2a
Madan <madanbeshir496@gmail.com>
2022-09-14 12:34:20 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* print alphabet - check the code for Holberton school students
+* print_alphabet - check the code for Holberton school students
 *
 *Return: Always 0.
 */
@@ -10,7 +10,7 @@ void print_alphabet(void)
 {
 	char alp = 'a';
 
-	while(alp <= 'z')
+	while (alp <= 'z')
 	{
 		_putchar (alp);
 		alp++;
