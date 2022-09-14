4d4daaa52484c3e22d6c593d51b01f675b234b11
Madan <madanbeshir496@gmail.com>
2022-09-14 12:09:24 -0700

    Task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include "stdio.h"
+
+/** 
+* print_to-98 - print all numbers provided between the provided number and 98
+* @n: an integer to start at
+* Return:Void.
+*/
+
+void print_to_98(int n)
+{
+	int i = 0;
+
+	if (n > 98)
+	{
+		for (i = n; i<=98; i--)
+		{
+			if (i != n)
+			{
+				printf(", ");
+			}
+			printf("%d", i);
+		}
+	}
+	else
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i != n)
+			{
+				printf(", ");
+			}
+			printf("%d", i);
+		}
+	}
+	_putchar('\n');
+}
