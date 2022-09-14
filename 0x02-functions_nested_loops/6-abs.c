71e7dd220940ccbabc7c6e782c8566abd1911fba
Madan <madanbeshir496@gmail.com>
2022-09-14 12:22:56 -0700

    Task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,8 +2,8 @@
 
 /**
 * _abs - returns absolute value of an int
-* @n - integer from which to get an absolute value
-* Return : Always 0.
+* @n: integer from which to get an absolute value
+* Return: Void.
 */
 
 int _abs(int n)
