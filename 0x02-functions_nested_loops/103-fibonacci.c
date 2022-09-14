9665e6c19389193cf3e57cea8a91d1c9f17b232f
Madan <madanbeshir496@gmail.com>
2022-09-14 14:13:52 -0700

    Task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* main - Entry point
+*
+* purpose - fibonnaci
+*
+*Return: (0) always
+*/
+
+int main(void)
+{
+	unsigned long int lim = 4000000;
+	unsigned long int bef = 0;
+	unsigned long int aft = 1;
+	unsigned long int R = 0;
+	unsigned long int sum = o;
+
+	while (lim > R)
+	{
+		R = bef + aft;
+
+		if ((R % 2) == 0)
+		{
+			sum += R;
+		}
+		bef = aft;
+		aft = R;
+	}
+	printf("%li\n", sum);
+	return (0);
+}
