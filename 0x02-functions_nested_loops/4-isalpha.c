86a0638f909475b0ea56ab396c398595d94a0f50
Madan <madanbeshir496@gmail.com>
2022-09-14 12:25:40 -0700

    Task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,14 +9,14 @@ Return: Always 0.
 */
 
 int _isalpha(int c)
-{ 
+{
 	if (c >= 97 && c <= 122)
 	{
-		return(1);
+		return (1);
 	}
-	else if (c >=65 && c <= 90)
+	else if (c >= 65 && c <= 90)
 	{
-		return(1);
+		return (1);
 	}
 	return (0);
-}  
+}
