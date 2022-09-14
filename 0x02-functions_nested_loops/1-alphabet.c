a010a2036548ea41c32b63eec17d66fd6dfc862b
Madan <madanbeshir496@gmail.com>
2022-09-14 07:06:59 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print alphabet - check the code for Holberton school students
+*
+*Return: Always 0.
+*/
+
+void print_alphabet(void)
+{
+	char alp = 'a';
+
+	while(alp <= 'z')
+	{
+		_putchar (alp);
+		alp++;
+
+	}
+	_putchar ('\n');
+}
