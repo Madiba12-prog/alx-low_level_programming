2a625e8569784a4011087dc084e4c1e9e9a3590e
Madan <madanbeshir496@gmail.com>
2022-09-16 05:14:10 -0700

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,20 +2,16 @@
 #include <stdio.h>
 
 /**
-* _isupper - check the code for Holberton School students.
-*@c : variable
-* Return: 0 or 1 .
+* _isupper - Check if paramter c represents an uppercase letter.
+* @c: Int representing a character
+* Return: Always 0.
 */
 
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
-	}
-	else
-	{
-		return (0);
 	}
 	return (0);
 }
