689667d7290f03e204a552836f9c0710d881204d
Madan <madanbeshir496@gmail.com>
2022-09-16 11:52:51 -0700

    Task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,51 +1,31 @@
 #include "main.h"
-
 #include <stdio.h>
 
-
-
 /**
- *
- *  * print_square - Print square line dependent on the integer n.
- *
- *   * @n : The number of lines using '#' characters to use per row and column
- *
- *    * Return: Void.
- *
- *     */
+* print_square - Print square line dependent on the integer n.
+* @n : The number of lines using '#' characters to use per row and column
+* Return: Void.
+*/
 
 void print_square(int n)
 
 {
+	int x;
 
-		int x;
+	int y;
 
-			int y;
-
-
-
-				for (y = 0; y < n; y++)
-
-						{
-
-									for (x = 0; x < n; x++)
-
-												{
-
-																_putchar('#');
-
-																		}
+	for (y = 0; y < n; y++)
+	{
+		for (x = 0; x < n; x++)								{
+					
+			_putchar('#');
+		}
 
 											_putchar('\n');
 
 												}
-
-					if (n <= 0)
-
-							{
-
-										_putchar('\n');
-
-											}
-
+		if (n <= 0)						
+		{
+									
+			_putchar('\n');								}
 }
