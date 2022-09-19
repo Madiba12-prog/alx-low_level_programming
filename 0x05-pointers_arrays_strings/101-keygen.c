126d530730ab53ed05a7ee7f03b466464e421420
Madan <madanbeshir496@gmail.com>
2022-09-19 05:34:54 -0700

    Task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,17 +9,28 @@
 
 int main(void)
 {
-	int sum;
-
-	char c;
+	int i, j, k, s;
+	char c[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char p[58];
 
 	srand(time(NULL));
-	while (sum <= 2645)
-	{
-		c = rand() % 128;
-		sum += c;
-		putchar(c);
-	}
-	putchar(2772 - sum);
+	while (s != 2772)
+	{											i = k = s = 0;
+		while ((2772 - 122) > s)
+		{											j = rand() % 62;				
+			p[i] = c[j];
+			s += c[j];
+			i++;									}		
+		while (c[k])		
+		{											if (c[k] == (2772 - s))								{
+																								p[i] = c[k];
+				s += c[k];	
+				i++;
+				break;
+			}
+			k++;									}
+											}
+	p[i] = '\0';
+	printf("%s", p);
 	return (0);
 }
