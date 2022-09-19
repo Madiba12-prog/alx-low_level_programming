0c0ef9e5b1d81d3420e9979f749949db79932880
Madan <madanbeshir496@gmail.com>
2022-09-19 04:36:52 -0700

    Task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* char *_strcpy - a function that copies the string pointed to by src
+* *_strcpy - a function that copies the string pointed to by src
 * @dest: copy to
 * @src: copy from
 * Return: string
@@ -9,11 +9,12 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+	int i = -1;
 
-	for (; src[i] != '\0'; i++)
-	{
+	do {
+		i++;
 		dest[i] = src[i];
-	}
+	} while (src[i] != '\0');
+
 	return (dest);
 }
