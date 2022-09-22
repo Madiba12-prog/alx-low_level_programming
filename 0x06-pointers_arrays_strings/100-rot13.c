4dc156395919cbd73647a91cff30b346963cf01e
Madan <madanbeshir496@gmail.com>
2022-09-22 07:21:53 -0700

    Task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -14,7 +14,8 @@ char *rot13(char *s)
 	while (s[a])
 	{
 		while ((s[a] >= 'a' && s[a] <= 'z') || (s[a] >= 'A' && s[a] <= 'Z'))
-		{											if ((s[a] > 'm' && s[a] <= 'z') || (s[a] > 'M' && s[a] <= 'Z'))			{
+		{											if ((s[a] > 'm' && s[a] <= 'z') || (s[a] > 'M' && s[a] <= 'Z'))		
+			{
 				s[a] -= 13;
 				break;
 			}
