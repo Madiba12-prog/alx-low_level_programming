2a8253a4b7504dd1076b7de11ba41feaf53a04f4
Madan <madanbeshir496@gmail.com>
2022-10-03 01:51:50 -0700

    Main

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,21 +1,24 @@
-#ifndef HEADERFILE_H
-
-#define HEADERFILE_H
-
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 #include <stdio.h>
-
 #include <stdlib.h>
 
 int _putchar(char c);
 
-void *malloc_checked(unsigned int b);
+char *create_array(unsigned int size, char c);
 
-char *string_nconcat(char *s1, char *s2, unsigned int n);
+char *_strdup(char *str);
 
-void *_calloc(unsigned int nmemb, unsigned int size);
+char *str_concat(char *s1, char *s2);
 
-int *array_range(int min, int max);
+int **alloc_grid(int width, int height);
 
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void free_grid(int **grid, int height);
 
-#endif
+char *argstostr(int ac, char **av);
+
+char **strtow(char *str);
+
+
+
+#endif 
