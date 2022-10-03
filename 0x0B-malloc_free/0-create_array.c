167ff062c647496ac8268ecc141aed6f4cb3bb5b
Madan <madanbeshir496@gmail.com>
2022-10-03 02:09:44 -0700

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -5,7 +5,8 @@
 * @size: the desired size of the array
 * @c: the value with which the array should be initialized
 * Return: NULL if size is 0 or memory allocation fails,
-* otherwise 0*/
+* otherwise 0
+*/
 
 char *create_array(unsigned int size, char c)
 {
